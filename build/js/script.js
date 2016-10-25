acad692f2190b3d5214a868a182bd9337d4f779f
Дмитрий Жур <tehzienergy@gmail.com>
2016-10-25 15:24:31 +0300

    add modals

--- build/js/script.js
+++ build/js/script.js
@@ -12,13 +12,13 @@ $(".fancybox").fancybox({});
 
 
 
+
+
+
+
+
+
 $(".header__mobile-button").click(function(e){e.preventDefault(),$(".header__mobile-menu").slideToggle("fast")}),$(".header__mobile-close").click(function(e){e.preventDefault(),$(".header__mobile-menu").slideToggle("fast")});
-
-
-
-
-
-
 
 
 
