20a39f8a1eb244ffe2f8f45b4b2f50b00c670c98
Дмитрий Жур <tehzienergy@gmail.com>
2016-10-25 13:32:28 +0300

    add fancybox src

--- build/js/script.js
+++ build/js/script.js
@@ -8,10 +8,14 @@ $(document).ready(function(){
 
 
 
+$(".fancybox").fancybox({});
 
 
 
 $(".header__mobile-button").click(function(e){e.preventDefault(),$(".header__mobile-menu").slideToggle("fast")}),$(".header__mobile-close").click(function(e){e.preventDefault(),$(".header__mobile-menu").slideToggle("fast")});
+
+
+
 
 
 
